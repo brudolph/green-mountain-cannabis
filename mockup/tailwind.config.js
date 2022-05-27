369eeddd3813bb4657a8f8b2fa88c396a6ede9e7
Ben Rudolph <momentumdd@me.com>
2022-05-27 12:18:52 -0600

    updated styling

--- mockup/tailwind.config.js
+++ mockup/tailwind.config.js
@@ -2,24 +2,30 @@ module.exports = {
   content: ['./src/**/*.html'],
   theme: {
     extend: {
+      fontFamily: {
+        headers: ['Oswald', 'Arial', 'sans-serif'],
+        sans: ['"Open Sans"', 'Arial', 'sans-serif'],
+      },
       colors: {
         primary: {
           DEFAULT: '#4A7639',
           light: '#729b62',
-          dark: '#2b511b'
+          dark: '#2b511b',
         },
-        secondary:'#85AC59',
-        accent: '#F3AB53',
+        secondary: '#85AC59',
+        accent: '#F0982E',
         text: '#272932',
         links: '#2b511b',
         dark: '#000000',
         light: '#e2e2e2',
         error: '#b81717',
         transparent: 'transparent',
-      }
+      },
     },
   },
-  variants: {
-  },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'),],
+  variants: {},
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
