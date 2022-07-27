d25311aa6e380584852cdd4a763122befea63d3a
Ben Rudolph <momentumdd@me.com>
2022-07-26 21:39:11 -0600

    Updates to cart functionality

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -1,25 +1,21 @@
 import { KeystoneContext } from '@keystone-6/core/types';
-import { Context } from './.keystone/types';
-import { CartItem } from '.prisma/client';
 import { Session } from '../types';
 
 async function addToCart(
   root: any,
-  { productId }: { productId: string },
-  context: Context
-): Promise<CartItem> {
-  console.log('ADDING TO CART!');
+  { productId, quantity }: { productId: string, quantity: string },
+  context: KeystoneContext
+): Promise<any> {
+
   // 1. Query the current user see if they are signed in
   const sesh = context.session as Session;
   if (!sesh.itemId) {
     throw new Error('You must be logged in to do this!');
   }
   // 2. Query the current users cart
-  const allCartItems = await context.db.CartItem.findMany({
-    where: {
-      user: { id: { equals: sesh.itemId } },
-      product: { id: { equals: productId } },
-    },
+  const allCartItems = await context.query.CartItem.findMany({
+    where: { user: { id: { equals: sesh.itemId } }, product: { id: { equals: productId } } },
+    query: 'id quantity',
   });
 
   const [existingCartItem] = allCartItems;
@@ -29,16 +25,17 @@ async function addToCart(
       `There are already ${existingCartItem.quantity}, increment by 1!`
     );
     // 3. See if the current item is in their cart
-    // 4. if itis, increment by 1
+    // if it is, increment by 1
     return context.db.CartItem.updateOne({
       where: { id: existingCartItem.id },
-      data: { quantity: existingCartItem.quantity + 1 },
+      data: { quantity: quantity },
     });
   }
   // 4. if it isnt, create a new cart item!
   return context.db.CartItem.createOne({
     data: {
       product: { connect: { id: productId } },
+      quantity: quantity,
       user: { connect: { id: sesh.itemId } },
     },
   });
