886483c28b53591c14f1522f85dbc71982e48d19
Ben Rudolph <momentumdd@me.com>
2022-08-11 09:01:12 -0600

    changing db config

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -25,6 +25,8 @@ export default withAuth(
     db: {
       provider: 'postgresql',
       url: databaseURL,
+      enableLogging: true,
+      useMigrations: true,
     },
     lists: ({
       User,
