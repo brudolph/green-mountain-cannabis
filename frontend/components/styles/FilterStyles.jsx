430649f6ae506ed94520dfcc67b9a9a5bb10cb10
Ben Rudolph <momentumdd@me.com>
2022-09-01 23:26:23 -0600

    Changes for product filtering

--- frontend/components/styles/FilterStyles.jsx
+++ frontend/components/styles/FilterStyles.jsx
@@ -1,6 +1,6 @@
 import tw, { styled } from 'twin.macro';
 
-export const FilterStyles = styled.div(tw`bg-white`);
+export const FilterStyles = styled.div(tw`bg-white z-0`);
 export const FiltersContainer = styled.div(
   tw`flex justify-between space-x-4 max-w-7xl mx-auto pb-4 pt-6 px-5`
 );
@@ -10,5 +10,5 @@ export const ActiveFiltersContainer = styled.div(
   tw`w-full max-w-7xl mx-auto py-4 px-5 sm:flex sm:items-center`
 );
 export const ActiveFiltersHeader = styled.h2(
-  tw`text-sm font-normal uppercase tracking-wide mr-4 text-gray-900`
+  tw`text-sm font-normal uppercase tracking-wide py-1 mr-4 text-gray-900`
 );
